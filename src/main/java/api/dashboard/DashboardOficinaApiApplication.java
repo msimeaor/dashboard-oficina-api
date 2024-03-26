f7bc67b3d34678fe7a7d71df5ce2c80c7178d17b
Matheus <maatsimeao@gmail.com>
2024-03-26 09:32:48 -0300

    Initial Commit
    Initial Commit

---
+++ src/main/java/api/dashboard/DashboardOficinaApiApplication.java
@@ -0,0 +1,13 @@
+package api.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardOficinaApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DashboardOficinaApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/api/dashboard/DashboardOficinaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardOficinaApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
