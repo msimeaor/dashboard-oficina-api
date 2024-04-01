bcfa2b0590e0ca26771c6e3866e09e4e80550915
Matheus <maatsimeao@gmail.com>
2024-04-01 11:31:27 -0300

    Injetando dependencias de anotacao
    Fiz a injeção de um AnotacaoService no controller de anotacao
    Fiz também a injeção do AnotacaoRepository no AnotacaoService

--- src/main/java/api/dashboard/controllers/AnotacaoRestController.java
+++ src/main/java/api/dashboard/controllers/AnotacaoRestController.java
@@ -1,5 +1,6 @@
 package api.dashboard.controllers;
 
+import api.dashboard.model.services.impl.AnotacaoServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/anotacoes")
 public class AnotacaoRestController {
 
-  public AnotacaoRestController() {}
+  private AnotacaoServiceImpl service;
+
+  public AnotacaoRestController(AnotacaoServiceImpl service) {
+    this.service = service;
+  }
 
 }
