4002512f34fdcf8e0e968a9f51be58d0566afd82
Matheus <maatsimeao@gmail.com>
2024-04-01 11:32:43 -0300

    Injetando dependencias de servico
    Fiz a injeção de um ServicoService no controller de servico
    Fiz também a injeção do ServicoRepository no ServicoService

--- src/main/java/api/dashboard/controllers/ServicoRestController.java
+++ src/main/java/api/dashboard/controllers/ServicoRestController.java
@@ -1,5 +1,6 @@
 package api.dashboard.controllers;
 
+import api.dashboard.model.services.impl.ServicoServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/servicos")
 public class ServicoRestController {
 
-  public ServicoRestController() {}
+  private ServicoServiceImpl service;
+
+  public ServicoRestController(ServicoServiceImpl service) {
+    this.service = service;
+  }
 
 }
--- src/main/java/api/dashboard/model/services/impl/ServicoServiceImpl.java
+++ src/main/java/api/dashboard/model/services/impl/ServicoServiceImpl.java
@@ -1,11 +1,16 @@
 package api.dashboard.model.services.impl;
 
+import api.dashboard.model.repositories.ServicoRepository;
 import api.dashboard.model.services.ServicoService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServicoServiceImpl implements ServicoService {
 
-  public ServicoServiceImpl() {}
+  private ServicoRepository repository;
+
+  public ServicoServiceImpl(ServicoRepository repository) {
+    this.repository = repository;
+  }
 
 }
