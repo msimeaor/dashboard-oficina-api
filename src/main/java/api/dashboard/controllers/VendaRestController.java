d9abbc016039d3df3f8e47401ca5f8ab2cac4f15
Matheus <maatsimeao@gmail.com>
2024-04-01 11:30:02 -0300

    Injetando dependencias de venda
    Fiz a injeção de um VendaService no controller de venda
    Fiz também a injeção do VendaRepository no VendaService

--- src/main/java/api/dashboard/controllers/VendaRestController.java
+++ src/main/java/api/dashboard/controllers/VendaRestController.java
@@ -1,5 +1,6 @@
 package api.dashboard.controllers;
 
+import api.dashboard.model.services.impl.VendaServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/vendas")
 public class VendaRestController {
 
-  public VendaRestController() {}
+  private VendaServiceImpl service;
+
+  public VendaRestController(VendaServiceImpl service) {
+    this.service = service;
+  }
 
 }
--- src/main/java/api/dashboard/model/services/impl/VendaServiceImpl.java
+++ src/main/java/api/dashboard/model/services/impl/VendaServiceImpl.java
@@ -1,11 +1,16 @@
 package api.dashboard.model.services.impl;
 
+import api.dashboard.model.repositories.VendaRepository;
 import api.dashboard.model.services.VendaService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VendaServiceImpl implements VendaService {
 
-  public VendaServiceImpl() {}
+  private VendaRepository repository;
+
+  public VendaServiceImpl(VendaRepository repository) {
+    this.repository = repository;
+  }
 
 }
