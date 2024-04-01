de2497287156ebced3af41545b6ec59d911f752c
Matheus <maatsimeao@gmail.com>
2024-04-01 11:00:32 -0300

    Criando implementacao dos services
    Criei a classe que vai implementar as interfaces services.
    Essa classe vai conter a implementação de cada método da sua respectiva interface.
    Aqui serão desenvolvidas as logicas de negócios do sistema.

---
+++ src/main/java/api/dashboard/model/services/impl/ServicoServiceImpl.java
@@ -0,0 +1,11 @@
+package api.dashboard.model.services.impl;
+
+import api.dashboard.model.services.ServicoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicoServiceImpl implements ServicoService {
+
+  public ServicoServiceImpl() {}
+
+}
---
+++ src/main/java/api/dashboard/model/services/impl/TelefoneServiceImpl.java
@@ -0,0 +1,11 @@
+package api.dashboard.model.services.impl;
+
+import api.dashboard.model.services.TelefoneService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TelefoneServiceImpl implements TelefoneService {
+
+  public TelefoneServiceImpl() {}
+
+}
