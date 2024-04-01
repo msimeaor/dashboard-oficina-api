146ddc6ade96703c7987881d0e6965787f002305
Matheus <maatsimeao@gmail.com>
2024-04-01 11:24:09 -0300

    Injetando dependencias de clientes
    Fiz a injeção de um ClienteService no controller de cliente
    Fiz também a injeção do ClienteRepository no ClienteService
    As injeções foram feitas por construtor, o SpringBoot irá gerenciar a instanciação da classe e o tempo de vida.

--- src/main/java/api/dashboard/controllers/ClienteRestController.java
+++ src/main/java/api/dashboard/controllers/ClienteRestController.java
@@ -1,5 +1,6 @@
 package api.dashboard.controllers;
 
+import api.dashboard.model.services.impl.ClienteServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/clientes")
 public class ClienteRestController {
 
-  public ClienteRestController() {}
+  private ClienteServiceImpl service;
+
+  public ClienteRestController(ClienteServiceImpl service) {
+    this.service = service;
+  }
 
 }
