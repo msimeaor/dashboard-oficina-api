0f807ce00bbba799f6f13e16c49eeba33d78a263
Matheus <maatsimeao@gmail.com>
2024-04-11 14:40:22 -0300

    Corrigindo erros nos repositorios
    O parametro da generic da classe JpaRepository desses dois repositorios estava sendo passado errado.

--- src/main/java/api/dashboard/model/repositories/AnotacaoRepository.java
+++ src/main/java/api/dashboard/model/repositories/AnotacaoRepository.java
@@ -1,8 +1,8 @@
 package api.dashboard.model.repositories;
 
-import api.dashboard.model.entities.Venda;
+import api.dashboard.model.entities.Anotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnotacaoRepository extends JpaRepository<Venda, Long> { }
+public interface AnotacaoRepository extends JpaRepository<Anotacao, Long> { }
