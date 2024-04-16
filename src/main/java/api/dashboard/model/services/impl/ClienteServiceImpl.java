0aa87eb79898f68f1d670e28f69a1b5052ebda48
Matheus <maatsimeao@gmail.com>
2024-04-16 14:13:06 -0300

    Logica do endpoint `getEstatisticasClientes` desenvolvida
    Desenvolvi o método na service de cliente que trata a lógica do endpoint para retornar estatísticas de cadastros de clientes.
    As estatísticas serão: Quantos clientes já foram cadastrados no sistema no geral e qual a porcentagem de crescimento de cadastro de clientes comparando o mês atual (últimos 30 ou 31 dias) e o mês anterior.

--- src/main/java/api/dashboard/model/services/impl/ClienteServiceImpl.java
+++ src/main/java/api/dashboard/model/services/impl/ClienteServiceImpl.java
@@ -1,22 +1,34 @@
 package api.dashboard.model.services.impl;
 
 import api.dashboard.model.dtos.response.EstatisticasDTO;
-import api.dashboard.model.repositories.ClienteRepository;
 import api.dashboard.model.services.ClienteService;
+import api.dashboard.utilities.Calculos;
+import api.dashboard.utilities.clientes.AcessoDadosCliente;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
-  private ClienteRepository repository;
+  private AcessoDadosCliente acessoDadosCliente;
 
-  public ClienteServiceImpl(ClienteRepository repository) {
-    this.repository = repository;
+  public ClienteServiceImpl(AcessoDadosCliente acessoDadosCliente) {
+    this.acessoDadosCliente = acessoDadosCliente;
   }
 
   public ResponseEntity<EstatisticasDTO> getEstatisticasClientes() {
-    return null;
+    Integer totalClientes = acessoDadosCliente.getTotalRegistrosCadastrados();
+    Double porcentagemCrescimentoClientesUltimoMes = new Calculos(acessoDadosCliente)
+            .calcPorcentagemCrescimentoRegistrosCadastradosUltimos30DiasEmRelacaoMesAnterior();
+
+    EstatisticasDTO estatisticasDTO = EstatisticasDTO.builder()
+            .nomeEntidade("Clientes")
+            .total(totalClientes)
+            .crescimento(porcentagemCrescimentoClientesUltimoMes)
+            .build();
+
+    return new ResponseEntity<>(estatisticasDTO, HttpStatus.OK);
   }
 
 }
