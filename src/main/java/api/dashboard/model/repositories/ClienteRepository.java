e1c65f24d134085502931f6d27021ead7a891770
Matheus <maatsimeao@gmail.com>
2024-04-16 14:20:21 -0300

    Queries JPQL do repository de cliente criadas
    Criei as queries JPQL que serão usadas para consultar a contagem de clientes no banco de dados levando em consideração um período de datas passadas.

--- src/main/java/api/dashboard/model/repositories/ClienteRepository.java
+++ src/main/java/api/dashboard/model/repositories/ClienteRepository.java
@@ -1,8 +1,26 @@
 package api.dashboard.model.repositories;
 
 import api.dashboard.model.entities.Cliente;
+import jakarta.websocket.server.PathParam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> { }
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+  @Query("SELECT COUNT(c) FROM Cliente c " +
+          "WHERE c.dataCriacao >= :umMesAtras")
+  Integer countClienteLast30DaysByDataCriacao(@PathParam("umMesAtras") LocalDate umMesAtras);
+
+  @Query("SELECT COUNT(c) FROM Cliente c " +
+          "WHERE c.dataCriacao >= :doisMesesAtras " +
+          "AND c.dataCriacao <= :umMesAtras")
+  Integer countClienteBetween2DatesByDataCriacao(@PathParam("umMesAtras") LocalDate doisMesesAtras,
+                                                 @PathParam("doisMesesAtras") LocalDate umMesAtras);
+
+  @Query("SELECT COUNT(*) FROM Cliente")
+  Integer countTotalClientesByDataCriacao();
+}
