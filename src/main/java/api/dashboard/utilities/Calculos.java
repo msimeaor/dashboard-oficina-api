edc8d727f5d00b6e8214061088876c690d84aacf
Matheus <maatsimeao@gmail.com>
2024-04-16 15:40:19 -0300

    Tratando erro de registros não encontrados
    Na logica do endpoint para mostrar as estatísticas de clientes, um calculo de porcentagem de crescimento é feito.
    Esse calculo é feito para obter a média de crescimento de cadastros de clientes no ultimo mês (últimos 30 dias) em relação ao penúltimo mês (últimos 30 dias anteriores ao ultimo mês). Porem, caso não hajam clientes cadastrados no penúltimo mês, a divisão por 0 fará uma exception ser lançada informando que não há registros cadastrados no ultimo mês.
    
    Criei um exception handler que vai ser responsável por capturar todas as exceptions que forem lançadas pelo sistema e retornar uma response personalizada para o usuário.

--- src/main/java/api/dashboard/utilities/Calculos.java
+++ src/main/java/api/dashboard/utilities/Calculos.java
@@ -1,5 +1,6 @@
 package api.dashboard.utilities;
 
+import api.dashboard.exceptions.ZeroRegistrosEncontradosException;
 import api.dashboard.utilities.interfaces.CalculosInterface;
 import api.dashboard.utilities.interfaces.searches.AcessoDados;
 
@@ -14,9 +15,15 @@ public class Calculos implements CalculosInterface {
   public Double calcPorcentagemCrescimentoRegistrosCadastradosUltimos30DiasEmRelacaoMesAnterior() {
     Integer registrosCadastradosUltimoMes = acessoDados.getRegistrosCadastradosUltimos30Dias();
     Integer registrosCadastradosPenultimoMes = acessoDados.getRegistrosCadastradosEntreUltimos30E60Dias();
+    validarRegistrosCadastradosPenultimoMes(registrosCadastradosPenultimoMes);
     Integer diferencaDeRegistros2Meses = registrosCadastradosUltimoMes - registrosCadastradosPenultimoMes;
 
     return (double) (diferencaDeRegistros2Meses * 100) / registrosCadastradosPenultimoMes;
+  }
+
+  private void validarRegistrosCadastradosPenultimoMes(Integer registrosCadastradosPenultimoMes) {
+    if (registrosCadastradosPenultimoMes == 0)
+      throw new ZeroRegistrosEncontradosException("Não há registros cadastrados no penúltimo mês!");
   }
 
 }
