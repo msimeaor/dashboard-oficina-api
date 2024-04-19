4501fece40b7668b74240567a7d1664d1c916fb7
Matheus <maatsimeao@gmail.com>
2024-04-19 10:23:46 -0300

    Criando exception ZeroCountException
    Essa exception irá ser lançada em validações da classe Calculo sempre que o total de registros retornados pelo COUNT for igual a zero.
    A principio, ela irá ser lançada somente nessa classe.

--- src/main/java/api/dashboard/exceptions/controllers/ExceptionHandlerController.java
+++ src/main/java/api/dashboard/exceptions/controllers/ExceptionHandlerController.java
@@ -1,9 +1,11 @@
 package api.dashboard.exceptions.controllers;
 
 import api.dashboard.exceptions.ExceptionResponse;
+import api.dashboard.exceptions.ZeroCountException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -13,6 +15,11 @@ import java.util.Date;
 @RestController
 @ControllerAdvice
 public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(ZeroCountException.class)
+  public ResponseEntity<ExceptionResponse> zeroCountException(Exception ex, WebRequest request) {
+    return criarERetornarExceptionResponse(ex, request, HttpStatus.NOT_FOUND);
+  }
 
   public ResponseEntity<ExceptionResponse> criarERetornarExceptionResponse(Exception ex,
                                                                            WebRequest request,
