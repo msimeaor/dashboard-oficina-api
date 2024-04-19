03c91a401805317e29cf8f89893f0742f05868bb
Matheus <maatsimeao@gmail.com>
2024-04-19 11:47:16 -0300

    Criando testes unitários endpoint getEstatisticasClientesByMes
    Os testes unitários validam o método do service de clientes que possui a logica do endpoint getEstatisticasClientesByMes
    São dois métodos de teste que validam os dois possíveis cenários ao chamar o endpoint.
    O primeiro cenário é de sucesso, onde as estatísticas são retornadas normalmente.
    O segundo é a falha. A API retorna uma exception quando o mês ao qual o usuário quer consultar as estatísticas de cadastros de clientes não possui nenhum cliente cadastrado. Dessa forma é impossível calcular o crescimento de clientes cadastrados no ultimo mês em relação a quantidade do mês filtrado.

--- src/test/java/api/dashboard/unittests/services/ClienteServiceImplTest.java
+++ src/test/java/api/dashboard/unittests/services/ClienteServiceImplTest.java
@@ -1,5 +1,6 @@
 package api.dashboard.unittests.services;
 
+import api.dashboard.exceptions.ZeroCountException;
 import api.dashboard.model.dtos.response.EstatisticasDTO;
 import api.dashboard.model.services.impl.ClienteServiceImpl;
 import api.dashboard.utilities.clientes.AcessoDadosCliente;
@@ -14,7 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
@@ -43,6 +44,38 @@ class ClienteServiceImplTest {
     assertEquals(100, content.getBody().getTotal());
     // O calculo do crescimento é: (registrosCadastradosUltimoMes * 100) / totalRegistros
     assertEquals(20.0d, content.getBody().getCrescimento());
+  }
+
+  @Test
+  void whenGetEstatisticasClientesByMesThenReturnSuccess() {
+    when(acessoDadosCliente.getRegistrosCadastradosUltimoMes()).thenReturn(20);
+    when(acessoDadosCliente.getRegistrosCadastradosMesEspecifico(anyInt())).thenReturn(15);
+    var content = service.getEstatisticasClientesByMes(1);
+
+    assertEquals(HttpStatus.OK, content.getStatusCode());
+    assertEquals(EstatisticasDTO.class, content.getBody().getClass());
+    assertEquals("Clientes", content.getBody().getNomeEntidade());
+    assertEquals(15, content.getBody().getTotal());
+    // O calculo do crescimento é: ((totalCadastrosUltimoMes - totalCadastrosMesEspecifico) * 100) / totalCadastrosMesEspecifico
+    assertEquals(33.333333333333336d, content.getBody().getCrescimento());
+  }
+
+  @Test
+  void whenGetEstatisticasClientesByMesThenReturnZeroCountException() {
+    when(acessoDadosCliente.getRegistrosCadastradosUltimoMes()).thenReturn(20);
+    when(acessoDadosCliente.getRegistrosCadastradosMesEspecifico(anyInt())).thenReturn(0);
+
+    try {
+      service.getEstatisticasClientesByMes(1);
+    } catch (Exception ex) {
+      assertEquals(ZeroCountException.class, ex.getClass());
+      assertEquals("Dados insuficientes!", ex.getMessage());
+    }
+
+    /*
+    A exception é lançada sempre que o total de clientes cadastrados no mês filtrado for igual a 0.
+    Neste caso é impossivel calcular o crescimento de cadastros.
+    */
   }
 
   public void startEntities() {}
