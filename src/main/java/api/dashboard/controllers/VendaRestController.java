875c739c4d326c351be90a656ba65c3ea4edac1f
Matheus <maatsimeao@gmail.com>
2024-04-22 10:37:29 -0300

    Criando endpoint getEstatisticasVendas
    Criei a lógica do endpoint que segue a mesma lógica do endpoint getEstatisticasClientes.
    A API recebe a requisição, então busca o numero total de vendas cadastradas no banco de dados e o numero de vendas cadastradas no ultimo mês.
    Um calculo é feito comparando o crescimento do ultimo mês em relação ao total de vendas cadastradas e então os valores são retornados na response.

--- src/main/java/api/dashboard/controllers/VendaRestController.java
+++ src/main/java/api/dashboard/controllers/VendaRestController.java
@@ -1,6 +1,9 @@
 package api.dashboard.controllers;
 
+import api.dashboard.model.dtos.response.EstatisticasDTO;
 import api.dashboard.model.services.impl.VendaServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,11 @@ public class VendaRestController {
 
   public VendaRestController(VendaServiceImpl service) {
     this.service = service;
+  }
+
+  @GetMapping("/buscas/getEstatisticasVendas")
+  public ResponseEntity<EstatisticasDTO> getEstatisticasVendas() {
+    return service.getEstatisticasVendas();
   }
 
 }
