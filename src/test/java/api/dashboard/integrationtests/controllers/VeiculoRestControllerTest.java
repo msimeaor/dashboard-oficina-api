fff8be74aa5e1c520a37487aa674b1af1902effd
Matheus <maatsimeao@gmail.com>
2024-04-23 11:48:09 -0300

    Criando testes de integração endpoint getEstatisticasVeiculosByMes
    Criei os dois testes de integração do endpoint.
    Os testes validam ambos caminhos, tanto de sucesso, quanto de falha.
    No sucesso, dados como: Total de veículos e crescimento no ultimo mês são validados.
    Na falha, não são retornados nenhum veiculo no mês selecionado. Sendo assim, o calculo do crescimento não pode ser feito.

--- src/test/java/api/dashboard/integrationtests/controllers/VeiculoRestControllerTest.java
+++ src/test/java/api/dashboard/integrationtests/controllers/VeiculoRestControllerTest.java
@@ -1,6 +1,7 @@
 package api.dashboard.integrationtests.controllers;
 
 import api.dashboard.configs.TestConfigs;
+import api.dashboard.exceptions.ExceptionResponse;
 import api.dashboard.integrationtests.testcontainers.AbstractIntegrationTests;
 import api.dashboard.model.dtos.response.EstatisticasDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,6 +11,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,8 +21,8 @@ import static io.restassured.RestAssured.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class VeiculoRestControllerTest extends AbstractIntegrationTests {
 
-  private RequestSpecification specification;
-  private ObjectMapper mapper;
+  private static RequestSpecification specification;
+  private static ObjectMapper mapper;
 
   @BeforeEach
   void setUp() {
@@ -64,4 +66,60 @@ class VeiculoRestControllerTest extends AbstractIntegrationTests {
     Sendo assim, o calculo ficaria (veiculosCadastradosUltimoMes * 100) / totalVeiculos = 20.0
     */
   }
+
+  @Test
+  @Order(2)
+  void getEstatisticasVeiculosByMes() throws JsonProcessingException {
+    var content = given().spec(specification)
+            .basePath("/api/veiculos/buscas/getEstatisticasVeiculosByMes")
+            .param("mes", 1)
+            .when()
+              .get()
+            .then()
+              .statusCode(200)
+            .extract()
+              .body()
+                .asString();
+
+    var response = mapper.readValue(content, EstatisticasDTO.class);
+    response.setCrescimento(-33.333333333333336d);
+
+    assertEquals(EstatisticasDTO.class, response.getClass());
+    assertEquals("Veículos", response.getNomeEntidade());
+    assertEquals(15, response.getTotal());
+    assertEquals(-33.333333333333336d, response.getCrescimento());
+    /*
+    Para realizar o calculo do crescimento, a API busca os registros cadastrados nos ultimos 30 dias no banco de dados
+    Ou seja, se eu chamar esse endpoint hoje (23/04/2024), a API busca registros cadastrados de 23/03/2024 - hoje.
+    Se eu rodar daqui 1 mes, o range de datas muda, fazendo com que o valor retornado mude e o resultado do calculo
+    fique diferente, alterando também o valor do crescimento.
+    Para isso, peguei valores de hoje e setei estaticamente no teste.
+    Veículos cadastradas no mês 1 = 15
+    Veículos buscados de 23/03/2024 - hoje = 10
+    Crescimento dos ultimos 30 dias em relação ao mês 1 = -33.333333333333336%
+    */
+  }
+
+  @Test
+  @Order(3)
+  void getEstatisticasVeiculosByMesThrownZeroCountException() throws JsonProcessingException {
+    var content = given().spec(specification)
+            .basePath("/api/veiculos/buscas/getEstatisticasVeiculosByMes")
+            .param("mes", 5)
+            .when()
+              .get()
+            .then()
+              .statusCode(404)
+            .extract()
+              .body()
+                .asString();
+
+    var response = mapper.readValue(content, ExceptionResponse.class);
+
+    assertEquals(ExceptionResponse.class, response.getClass());
+    assertEquals(HttpStatus.NOT_FOUND.value(), response.getCodigoHttpErro());
+    assertEquals("Dados insuficientes!", response.getMensagemErro());
+    assertEquals("uri=/api/veiculos/buscas/getEstatisticasVeiculosByMes", response.getDetalhesErro());
+  }
+
 }
