491c957caced1d14a30a9ea35f35ce0f4dca2f66
Matheus <maatsimeao@gmail.com>
2024-04-23 15:35:26 -0300

    Criando testes unitários do endpoint GetEstatisticasVeiculosByMes
    Os testes unitários validam o retorno do método GetEstatisticasVeiculosByMes no service de veiculo.
    O retorno do método é basicamente o retorno do endpoint no controller. Então os testes validam duas chamadas no endpoint.

--- src/test/java/api/dashboard/unittests/services/VeiculoServiceImplTest.java
+++ src/test/java/api/dashboard/unittests/services/VeiculoServiceImplTest.java
@@ -1,5 +1,6 @@
 package api.dashboard.unittests.services;
 
+import api.dashboard.exceptions.ZeroCountException;
 import api.dashboard.model.dtos.response.EstatisticasDTO;
 import api.dashboard.model.services.impl.VeiculoServiceImpl;
 import api.dashboard.utilities.searches.AcessoDadosVeiculos;
@@ -43,6 +44,39 @@ class VeiculoServiceImplTest {
     assertEquals(50, content.getBody().getTotal());
     // O calculo do crescimento é: (registrosCadastradosUltimoMes * 100) / totalRegistros
     assertEquals(20.0d, content.getBody().getCrescimento());
+  }
+
+  @Test
+  void whenGetEstatisticasVeiculosByMesThenReturnSuccess() {
+    when(acessoDadosVeiculos.getRegistrosCadastradosUltimoMes()).thenReturn(10);
+    when(acessoDadosVeiculos.getRegistrosCadastradosMesEspecifico(anyInt())).thenReturn(15);
+    var content = service.getEstatisticasVeiculosByMes(1);
+
+    assertEquals(HttpStatus.OK, content.getStatusCode());
+    assertEquals(EstatisticasDTO.class, content.getBody().getClass());
+    assertEquals("Veículos", content.getBody().getNomeEntidade());
+    assertEquals(15, content.getBody().getTotal());
+    // O calculo do crescimento é: ((totalCadastrosUltimoMes - totalCadastrosMesEspecifico) * 100) / totalCadastrosMesEspecifico
+    assertEquals(-33.333333333333336d, content.getBody().getCrescimento());
+  }
+
+  @Test
+  void whenGetEstatisticasVeiculosByMesThenThrownZeroCountException() {
+    when(acessoDadosVeiculos.getRegistrosCadastradosUltimoMes()).thenReturn(10);
+    when(acessoDadosVeiculos.getRegistrosCadastradosMesEspecifico(anyInt())).thenReturn(0);
+
+    try {
+      service.getEstatisticasVeiculosByMes(5);
+    } catch (Exception ex) {
+      assertEquals(ZeroCountException.class, ex.getClass());
+      assertEquals("Dados insuficientes!", ex.getMessage());
+    }
+
+    /*
+    A exception é lançada sempre que o total de veiculos cadastrados no mês filtrado for igual a 0.
+    Neste caso é impossivel calcular o crescimento de cadastros.
+    */
+
   }
 
   public void startEntities() {}
